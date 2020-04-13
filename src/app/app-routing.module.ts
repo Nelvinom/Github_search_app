c6af96402e933135fadc080ca88554982b09b38f
owinolawrence <owinolawrence21@gmail.com>
2020-04-13 19:05:39 +0300

    commit new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchModule } from './search/search.module';
 
 const routes: Routes =[
   {path: "", loadChildren:() => import("./search/search.module"). then(m => m.SearchModule) }
  ]
 
 @NgModule({
-  declarations: [],
   imports: [
   RouterModule.forRoot(routes, {scrollPositionRestoration: "enabled"})
   ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './search-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'; 
 import{ NgxPaginationModule } from 'ngx-pagination'
